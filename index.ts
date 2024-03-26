9bb96abdc4d7bb5480e1a3db32132e6f176b327b
Ayaz Ahmed <160207511+AhmedAyaz789@users.noreply.github.com>
2024-03-26 00:36:51 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const random =Math.floor(Math.random()*7+1);
+const answers = await inquirer.prompt([{
+ message:"Please guess a number between 1 - 7 : ",
+ type:"number" ,
+ name:"Guessednumber",
+}]);
+if(answers.Guessednumber=== random){
+  console.log("Congratulations you guessed a right number");
+  console.log("Author : Engineer Ayaz Ahmed Laghari");
+}else{
+  console.log("You Guessed wrong number");
+}
